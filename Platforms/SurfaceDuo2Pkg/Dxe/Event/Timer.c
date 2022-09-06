07faf2fc59f80ca11be1a41f8317954cd7ea8685
Gustave Monce <gustave.monce@outlook.com>
2022-09-06 21:48:50 +0200

    Zeta: Adjust timer hack again

--- Platforms/SurfaceDuo2Pkg/Dxe/Event/Timer.c
+++ Platforms/SurfaceDuo2Pkg/Dxe/Event/Timer.c
@@ -289,7 +289,7 @@ CoreSetTimer (
 
   DEBUG((EFI_D_ERROR, "CoreSetTimer Called!\n"));
   if (Type != TimerCancel && Type != TimerPeriodic) {
-    if (CountPassthroughTimer != 1) {
+    if (CountPassthroughTimer != 0) {
       CoreSignalEvent(UserEvent);
     }
     CountPassthroughTimer++;
