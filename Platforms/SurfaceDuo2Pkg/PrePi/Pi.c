7a5083786a47f740f6a965eb4d6cee5eba352293
Gustave Monce <gustave.monce@outlook.com>
2022-09-06 19:21:23 +0200

    Zeta: Disable Watchdog

--- Platforms/SurfaceDuo2Pkg/PrePi/Pi.c
+++ Platforms/SurfaceDuo2Pkg/PrePi/Pi.c
@@ -179,6 +179,10 @@ VOID Main(IN VOID *StackBase, IN UINTN StackSize, IN VOID *KernelLoadAddress, IN
     }
   }
 
+  DEBUG((EFI_D_INFO | EFI_D_LOAD, "Disabling Qualcomm Watchdog Reboot timer\n"));
+  MmioWrite32(0x17C10008, 0x00000000);
+  DEBUG((EFI_D_INFO | EFI_D_LOAD, "Qualcomm Watchdog Reboot timer disabled\n"));
+
   // Set up HOB
   HobList = HobConstructor(
       (VOID *)UefiMemoryBase, UefiMemorySize, (VOID *)UefiMemoryBase,
