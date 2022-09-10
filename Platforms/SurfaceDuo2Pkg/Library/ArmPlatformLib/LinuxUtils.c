887fce4edd75f8c302f7b55b14fc4e1f07ee282f
Gustave Monce <gustave.monce@outlook.com>
2022-09-10 22:21:20 +0200

    ArmPlatform: Remove Deprecated relocation code

--- Platforms/SurfaceDuo2Pkg/Library/ArmPlatformLib/LinuxUtils.c
+++ Platforms/SurfaceDuo2Pkg/Library/ArmPlatformLib/LinuxUtils.c
@@ -10,15 +10,20 @@ typedef VOID (*LINUX_KERNEL) (UINT64 ParametersBase,
 
 BOOLEAN IsLinuxAvailable(IN VOID *KernelLoadAddress)
 {
-  VOID *LinuxKernelAddr = KernelLoadAddress + 0x40 + PcdGet32(PcdFdSize);
+  VOID *LinuxKernelAddr = KernelLoadAddress + PcdGet32(PcdFdSize);
   UINT32 *LinuxKernelMagic = (UINT32*)(LinuxKernelAddr + LINUX_KERNEL_ARCH_MAGIC_OFFSET);
   return *LinuxKernelMagic == LINUX_KERNEL_AARCH64_MAGIC;
 }
 
 VOID BootLinux(IN VOID *DeviceTreeLoadAddress, IN VOID *KernelLoadAddress)
 {
-  VOID *LinuxKernelAddr = KernelLoadAddress + 0x40 + PcdGet32(PcdFdSize);
+  VOID *LinuxKernelAddr = KernelLoadAddress + PcdGet32(PcdFdSize);
   LINUX_KERNEL LinuxKernel = (LINUX_KERNEL) LinuxKernelAddr;
+
+  DEBUG(
+      (EFI_D_INFO | EFI_D_LOAD,
+       "Kernel Load Address = 0x%llx, Device Tree Load Address = 0x%llx\n",
+       LinuxKernelAddr, DeviceTreeLoadAddress));
 
   // Jump to linux
   LinuxKernel ((UINT64)DeviceTreeLoadAddress, 0, 0, 0);
@@ -30,6 +35,11 @@ VOID BootLinux(IN VOID *DeviceTreeLoadAddress, IN VOID *KernelLoadAddress)
 VOID ContinueToLinuxIfAllowed(IN VOID *DeviceTreeLoadAddress, IN VOID *KernelLoadAddress)
 {
   if (IsLinuxAvailable(KernelLoadAddress)) {
+    DEBUG(
+        (EFI_D_INFO | EFI_D_LOAD,
+        "Kernel Load Address = 0x%llx, Device Tree Load Address = 0x%llx\n",
+        KernelLoadAddress, DeviceTreeLoadAddress));
+
     if (IsLinuxBootRequested()) {
       BootLinux(DeviceTreeLoadAddress, KernelLoadAddress);
     }
