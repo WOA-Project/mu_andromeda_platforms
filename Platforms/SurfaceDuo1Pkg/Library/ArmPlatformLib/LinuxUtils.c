5bd5cfd0a492eb228bf93aad02709e9d238acd9c
Gustave Monce <gustave.monce@outlook.com>
2022-09-10 09:10:27 +0200

    ArmPlatform: Maybe this instead?

--- Platforms/SurfaceDuo1Pkg/Library/ArmPlatformLib/LinuxUtils.c
+++ Platforms/SurfaceDuo1Pkg/Library/ArmPlatformLib/LinuxUtils.c
@@ -20,11 +20,6 @@ VOID BootLinux(IN VOID *DeviceTreeLoadAddress, IN VOID *KernelLoadAddress)
   VOID *LinuxKernelAddr = KernelLoadAddress + 0x40 + PcdGet32(PcdFdSize);
   LINUX_KERNEL LinuxKernel = (LINUX_KERNEL) LinuxKernelAddr;
 
-  DEBUG(
-      (EFI_D_INFO | EFI_D_LOAD,
-       "Kernel Load Address = 0x%llx, Device Tree Load Address = 0x%llx\n",
-       LinuxKernelAddr, DeviceTreeLoadAddress));
-
   // Jump to linux
   LinuxKernel ((UINT64)DeviceTreeLoadAddress, 0, 0, 0);
 
@@ -35,11 +30,6 @@ VOID BootLinux(IN VOID *DeviceTreeLoadAddress, IN VOID *KernelLoadAddress)
 VOID ContinueToLinuxIfAllowed(IN VOID *DeviceTreeLoadAddress, IN VOID *KernelLoadAddress)
 {
   if (IsLinuxAvailable(KernelLoadAddress)) {
-    DEBUG(
-        (EFI_D_INFO | EFI_D_LOAD,
-        "Kernel Load Address = 0x%llx, Device Tree Load Address = 0x%llx\n",
-        KernelLoadAddress, DeviceTreeLoadAddress));
-
     if (IsLinuxBootRequested()) {
       BootLinux(DeviceTreeLoadAddress, KernelLoadAddress);
     }
