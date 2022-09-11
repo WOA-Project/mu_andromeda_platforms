7fb834b152855b2b16d173a08ccb19930ed39c4a
Gustave Monce <gustave.monce@outlook.com>
2022-09-11 11:50:38 +0200

    Zeta: Initialize UART even earlier for testing

--- Platforms/SurfaceDuo2Pkg/Library/ArmPlatformLib/LinuxUtils.c
+++ Platforms/SurfaceDuo2Pkg/Library/ArmPlatformLib/LinuxUtils.c
@@ -34,6 +34,9 @@ VOID BootLinux(IN VOID *DeviceTreeLoadAddress, IN VOID *KernelLoadAddress)
 
 VOID ContinueToLinuxIfAllowed(IN VOID *DeviceTreeLoadAddress, IN VOID *KernelLoadAddress)
 {
+  // Early Test..
+  PlatformInitialize();
+  
   if (IsLinuxAvailable(KernelLoadAddress)) {
     DEBUG(
         (EFI_D_INFO | EFI_D_LOAD,
