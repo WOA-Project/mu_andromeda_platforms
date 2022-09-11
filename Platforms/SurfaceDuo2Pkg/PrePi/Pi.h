5b58f7b1c2443f0241a7a4bf4b37b6a371854b93
Gustave Monce <gustave.monce@outlook.com>
2022-09-11 10:26:29 +0200

    Zeta: Test Arm PrePi's again

--- Platforms/SurfaceDuo2Pkg/PrePi/Pi.h
+++ Platforms/SurfaceDuo2Pkg/PrePi/Pi.h
@@ -19,6 +19,7 @@
 #include <Library/MemoryAllocationLib.h>
 #include <Library/HobLib.h>
 #include <Library/SerialPortLib.h>
+#include <Library/ArmPlatformLib.h>
 
 extern UINT64  mSystemMemoryEnd;
 
@@ -26,6 +27,13 @@ RETURN_STATUS
 EFIAPI
 TimerConstructor (
   VOID
+  );
+
+VOID
+PrePiMain (
+  IN  UINTN   UefiMemoryBase,
+  IN  UINTN   StacksBase,
+  IN  UINT64  StartTimeStamp
   );
 
 EFI_STATUS
@@ -47,37 +55,16 @@ BuildMemoryTypeInformationHob (
   VOID
   );
 
-VOID ContinueToLinuxIfAllowed(IN VOID *DeviceTreeLoadAddress, IN VOID *KernelLoadAddress);
-
-VOID QGicCpuInit(VOID);
-VOID QgicCpuInitSecondary(VOID);
-
-UINTN EFIAPI ArmGicAcknowledgeInterrupt(
-    IN UINTN GicInterruptInterfaceBase, OUT UINTN *InterruptId);
-VOID EFIAPI ArmGicEnableInterruptInterface(IN INTN GicInterruptInterfaceBase);
-
-VOID EFIAPI
-ArmGicEndOfInterrupt(IN UINTN GicInterruptInterfaceBase, IN UINTN Source);
-
-UINTN EFIAPI ArmGicGetMaxNumInterrupts(IN INTN GicDistributorBase);
-
-BOOLEAN IsLinuxBootRequested(VOID);
-VOID PlatformInitialize();
-
 EFI_STATUS
-EFIAPI
-QGicPeim(VOID);
+GetPlatformPpi (
+  IN  EFI_GUID  *PpiGuid,
+  OUT VOID      **Ppi
+  );
 
-#pragma pack(1)
-typedef struct {
-  /* First 2KB is reserved for OS */
-  UINT32 ProcessorId;
-  UINT32 Reserved;
-  UINT64 JumpAddress;
-  UINT8  OsReserved[2032];
-  /* Next 2KB is reserved for firmware */
-  UINT64 El2JumpFlag;
-} EFI_PROCESSOR_MAILBOX, *PEFI_PROCESSOR_MAILBOX;
-#pragma pack()
+VOID
+EFIAPI
+ProcessLibraryConstructorList (
+  VOID
+  );
 
 #endif /* _PREPI_H_ */
