c26c9ca78fc477e4b72cb406efade74173ea2e9e
Gustave Monce <gustave.monce@outlook.com>
2022-09-14 10:33:05 +0200

    Epsilon: PrePi: Cleanup

--- Platforms/SurfaceDuo1Pkg/PrePi/LinuxUtils.c
+++ Platforms/SurfaceDuo1Pkg/PrePi/LinuxUtils.c
@@ -1,12 +1,6 @@
 #include "Pi.h"
-
-typedef VOID (*LINUX_KERNEL) (UINT64 ParametersBase,
-                              UINT64 Reserved0,
-                              UINT64 Reserved1,
-                              UINT64 Reserved2);
-
-#define LINUX_KERNEL_ARCH_MAGIC_OFFSET 0x38
-#define LINUX_KERNEL_AARCH64_MAGIC 0x644D5241
+#include "PlatformUtils.h"
+#include "LinuxUtils.h"
 
 BOOLEAN IsLinuxAvailable(IN VOID *KernelLoadAddress)
 {
--- Platforms/SurfaceDuo1Pkg/PrePi/PlatformUtils.c
+++ Platforms/SurfaceDuo1Pkg/PrePi/PlatformUtils.c
@@ -1,26 +1,7 @@
 #include "Pi.h"
-
+#include "PlatformUtils.h"
+#include "EarlyQGic/EarlyQGic.h"
 #include <Configuration/DeviceMemoryMap.h>
-
-#define TLMM_WEST 0x03100000
-#define TLMM_EAST 0x03500000
-#define TLMM_NORTH 0x03900000
-#define TLMM_SOUTH 0x03D00000
-
-#define TLMM_ADDR_OFFSET_FOR_PIN(x) (0x1000 * x)
-
-#define TLMM_PIN_CONTROL_REGISTER 0
-#define TLMM_PIN_IO_REGISTER 4
-#define TLMM_PIN_INTERRUPT_CONFIG_REGISTER 8
-#define TLMM_PIN_INTERRUPT_STATUS_REGISTER 0xC
-#define TLMM_PIN_INTERRUPT_TARGET_REGISTER TLMM_PIN_INTERRUPT_CONFIG_REGISTER
-
-#define LID0_GPIO121_STATUS_ADDR                                               \
-  (TLMM_SOUTH + TLMM_ADDR_OFFSET_FOR_PIN(121) + TLMM_PIN_IO_REGISTER)
-
-#define MDSS_DSI0 0xAE94000
-#define MDSS_DSI1 0xAE96000
-#define DSI_CTRL 4
 
 BOOLEAN IsLinuxBootRequested()
 {
@@ -80,31 +61,50 @@ VOID UartInit(VOID)
        (CHAR16 *)PcdGetPtr(PcdFirmwareVersionString), __TIME__, __DATE__));
 }
 
+VOID ConfigureIOMMUContextBankCacheSetting(UINT32 ContextBankId, BOOLEAN CacheCoherent)
+{
+  UINT32 ContextBankAddr = SMMU_BASE + SMMU_CTX_BANK_0_OFFSET + ContextBankId * SMMU_CTX_BANK_SIZE;
+  MmioWrite32(ContextBankAddr + SMMU_CTX_BANK_SCTLR_OFFSET, CacheCoherent ? SMMU_CCA_SCTLR : SMMU_NON_CCA_SCTLR);
+  MmioWrite32(ContextBankAddr + SMMU_CTX_BANK_TTBR0_0_OFFSET, 0);
+  MmioWrite32(ContextBankAddr + SMMU_CTX_BANK_TTBR0_1_OFFSET, 0);
+  MmioWrite32(ContextBankAddr + SMMU_CTX_BANK_TTBR1_0_OFFSET, 0);
+  MmioWrite32(ContextBankAddr + SMMU_CTX_BANK_TTBR1_1_OFFSET, 0);
+  MmioWrite32(ContextBankAddr + SMMU_CTX_BANK_MAIR0_OFFSET, 0);
+  MmioWrite32(ContextBankAddr + SMMU_CTX_BANK_MAIR1_OFFSET, 0);
+  MmioWrite32(ContextBankAddr + SMMU_CTX_BANK_TTBCR_OFFSET, 0);
+}
+
+VOID DisableMDSSDSIController(UINT32 MdssDsiBase)
+{
+  UINT32 DsiControlAddr = MdssDsiBase + DSI_CTRL;
+  UINT32 DsiControlValue = MmioRead32(DsiControlAddr);
+  DsiControlValue &= ~(DSI_CTRL_ENABLE | DSI_CTRL_VIDEO_MODE_ENABLE | DSI_CTRL_COMMAND_MODE_ENABLE);
+  MmioWrite32(DsiControlAddr, DsiControlValue);
+}
+
+VOID SetWatchdogState(BOOLEAN Enable)
+{
+  MmioWrite32(APSS_WDT_BASE + APSS_WDT_ENABLE_OFFSET, Enable);
+}
+
 VOID PlatformInitialize()
 {
   UartInit();
 
-  // Reset MDSS DSI0 Controller
-  MmioWrite32(MDSS_DSI0 + DSI_CTRL, MmioRead32(MDSS_DSI0 + DSI_CTRL) & ~0x7);
+  // Disable MDSS DSI0 Controller
+  DisableMDSSDSIController(MDSS_DSI0);
 
-  // Reset MDSS DSI1 Controller
-  MmioWrite32(MDSS_DSI1 + DSI_CTRL, MmioRead32(MDSS_DSI1 + DSI_CTRL) & ~0x7);
+  // Disable MDSS DSI1 Controller
+  DisableMDSSDSIController(MDSS_DSI1);
 
   // Windows requires Cache Coherency for the UFS to work at its best
   // The UFS device is currently attached to the main IOMMU on Context Bank 1
   // (Previous firmware) But said configuration is non cache coherent compliant,
   // fix it.
-  MmioWrite32(0x15081000, 0x9F00E0);
-  MmioWrite32(0x15081020, 0x0);
-  MmioWrite32(0x15081024, 0x0);
-  MmioWrite32(0x15081028, 0x0);
-  MmioWrite32(0x1508102C, 0x0);
-  MmioWrite32(0x15081038, 0x0);
-  MmioWrite32(0x1508103C, 0x0);
-  MmioWrite32(0x15081030, 0x0);
+  ConfigureIOMMUContextBankCacheSetting(UFS_CTX_BANK, TRUE);
 
   // Disable WatchDog Timer
-  MmioWrite32(0x17C10008, 0x00000000);
+  SetWatchdogState(FALSE);
 
   // Initialize GIC
   /*if (!FixedPcdGetBool(PcdIsLkBuild)) {
