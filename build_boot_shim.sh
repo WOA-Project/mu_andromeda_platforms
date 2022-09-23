24a283028db1b47cb56bbb52f281f3525929073e
Gustave Monce <gustave.monce@outlook.com>
2022-09-23 14:03:06 +0200

    BootShim: Rework build system

--- build_boot_shim.sh
+++ build_boot_shim.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 cd BootShim
-make
+
+make UEFI_BASE=0x9FC00000 UEFI_SIZE=0x00300000
+mv BootShim.bin BootShim.Epsilon.bin
+
+make UEFI_BASE=0x9F000000 UEFI_SIZE=0x00500000
+mv BootShim.bin BootShim.Zeta.bin
+
 cd ..
