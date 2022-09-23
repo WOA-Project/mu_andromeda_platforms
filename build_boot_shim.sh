9bffc0f56fa3969ef21191aabdf0cf221f722d39
Gustave Monce <gustave.monce@outlook.com>
2022-09-23 14:20:02 +0200

    BootShim: Fix an issue after consecutive builds

--- build_boot_shim.sh
+++ build_boot_shim.sh
@@ -5,6 +5,9 @@ cd BootShim
 make UEFI_BASE=0x9FC00000 UEFI_SIZE=0x00300000
 mv BootShim.bin BootShim.Epsilon.bin
 
+rm BootShim.bin
+rm BootShim.elf
+
 make UEFI_BASE=0x9F000000 UEFI_SIZE=0x00500000
 mv BootShim.bin BootShim.Zeta.bin
 
