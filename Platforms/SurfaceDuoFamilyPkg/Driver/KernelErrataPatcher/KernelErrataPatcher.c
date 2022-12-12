1d29ebe41627b81d4bac0bf95568deee9b5f1d1e
Gustave Monce <gustave.monce@outlook.com>
2022-12-12 08:29:51 +0100

    Kernel Patcher: More fixes for latest builds

--- Platforms/SurfaceDuoFamilyPkg/Driver/KernelErrataPatcher/KernelErrataPatcher.c
+++ Platforms/SurfaceDuoFamilyPkg/Driver/KernelErrataPatcher/KernelErrataPatcher.c
@@ -134,6 +134,7 @@ KernelErrataPatcherExitBootServices(
     IN EFI_HANDLE ImageHandle, IN UINTN MapKey,
     IN PLOADER_PARAMETER_BLOCK loaderBlockX19,
     IN PLOADER_PARAMETER_BLOCK loaderBlockX20,
+    IN PLOADER_PARAMETER_BLOCK loaderBlockX24,
     IN EFI_PHYSICAL_ADDRESS    returnAddress)
 {
   // Might be called multiple times by winload in a loop failing few times
@@ -152,8 +153,14 @@ KernelErrataPatcherExitBootServices(
       ((EFI_PHYSICAL_ADDRESS)loaderBlock & 0xFFFFFFF000000000) == 0) {
     FirmwarePrint(
         L"Failed to find OslLoaderBlock! loaderBlock -> 0x%p\n", loaderBlock);
+    loaderBlock = loaderBlockX24;
+  }
+
+  if (loaderBlock == NULL ||
+      ((EFI_PHYSICAL_ADDRESS)loaderBlock & 0xFFFFFFF000000000) == 0) {
+    FirmwarePrint(
+        L"Failed to find OslLoaderBlock! loaderBlock -> 0x%p\n", loaderBlock);
     goto exit;
-    loaderBlock = loaderBlockX19;
   }
 
   EFI_PHYSICAL_ADDRESS PatternMatch = FindPattern(
