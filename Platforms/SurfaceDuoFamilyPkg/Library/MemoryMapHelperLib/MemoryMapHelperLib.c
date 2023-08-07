0542961433c3cda44f107a37139598da85e7b690
Gustave Monce <gustave.monce@outlook.com>
2023-08-07 12:51:44 +0200

    Common: Refactor MemoryHelperLib for Sec and Non Sec

--- Platforms/SurfaceDuoFamilyPkg/Library/MemoryMapHelperLib/MemoryMapHelperLib.c
+++ Platforms/SurfaceDuoFamilyPkg/Library/MemoryMapHelperLib/MemoryMapHelperLib.c
@@ -1,38 +1,63 @@
 #include <Library/BaseLib.h>
 #include <Library/MemoryMapHelperLib.h>
-#include <Library/PlatformMemoryMapLib.h>
+#include <Library/HobLib.h>
+#include <Library/PlatformHobs.h>
+
+STATIC GUID            gEfiShLibHobGuid = EFI_SHIM_LIBRARY_GUID;
+STATIC UefiCfgLibType *UefiCfgLib       = NULL;
+
+ShLibLoaderType EFIAPI *GetEfiShLibHob()
+{
+  EFI_HOB_GUID_TYPE *GuidHob;
+  UINT32           **ShLib;
+
+  GuidHob = GetFirstGuidHob(&gEfiShLibHobGuid);
+
+  if (GuidHob == NULL) {
+    return NULL;
+  }
+
+  ShLib = GET_GUID_HOB_DATA(GuidHob);
+
+  return (ShLibLoaderType *)*ShLib;
+}
+
+EFI_STATUS EFIAPI GetUefiCfgLib()
+{
+  ShLibLoaderType *ShLib;
+
+  ShLib = GetEfiShLibHob();
+  if (ShLib == NULL) {
+    return EFI_NOT_READY;
+  }
+
+  return ShLib->LoadLib("UEFI Config Lib", 0, (VOID *)&UefiCfgLib);
+}
 
 EFI_STATUS EFIAPI LocateMemoryMapAreaByName(
     CHAR8 *MemoryMapAreaName, ARM_MEMORY_REGION_DESCRIPTOR_EX *MemoryDescriptor)
 {
-  PARM_MEMORY_REGION_DESCRIPTOR_EX MemoryDescriptorEx = GetPlatformMemoryMap();
-
-  // Run through each memory descriptor
-  while (MemoryDescriptorEx->Length != 0) {
-    if (AsciiStriCmp(MemoryMapAreaName, MemoryDescriptorEx->Name) == 0) {
-      *MemoryDescriptor = *MemoryDescriptorEx;
-      return EFI_SUCCESS;
+  if (UefiCfgLib == NULL) {
+    GetUefiCfgLib();
+    if (UefiCfgLib == NULL) {
+      return EFI_NOT_READY;
     }
-    MemoryDescriptorEx++;
   }
 
-  return EFI_NOT_FOUND;
+  return UefiCfgLib->GetMemInfoByName(MemoryMapAreaName, MemoryDescriptor);
 }
 
 EFI_STATUS EFIAPI LocateMemoryMapAreaByAddress(
     EFI_PHYSICAL_ADDRESS             MemoryMapAreaAddress,
     ARM_MEMORY_REGION_DESCRIPTOR_EX *MemoryDescriptor)
 {
-  PARM_MEMORY_REGION_DESCRIPTOR_EX MemoryDescriptorEx = GetPlatformMemoryMap();
-
-  // Run through each memory descriptor
-  while (MemoryDescriptorEx->Length != 0) {
-    if (MemoryDescriptorEx->Address == MemoryMapAreaAddress) {
-      *MemoryDescriptor = *MemoryDescriptorEx;
-      return EFI_SUCCESS;
+  if (UefiCfgLib == NULL) {
+    GetUefiCfgLib();
+    if (UefiCfgLib == NULL) {
+      return EFI_NOT_READY;
     }
-    MemoryDescriptorEx++;
   }
 
-  return EFI_NOT_FOUND;
+  return UefiCfgLib->GetMemInfoByAddress(
+      MemoryMapAreaAddress, MemoryDescriptor);
 }
