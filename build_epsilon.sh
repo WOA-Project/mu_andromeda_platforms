1843ee5e55221c999a8247eb97f45f699db1e2b2
Gustave Monce <gustave.monce@outlook.com>
2023-09-17 16:55:09 +0200

    Project: Individual build scripts

---
+++ build_epsilon.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+export CLANG38_BIN=/usr/lib/llvm-14/bin/ && export CLANG38_AARCH64_PREFIX=aarch64-linux-gnu-
+
+python3 ./Platforms/SurfaceDuo1Pkg/PlatformBuild.py --build TOOL_CHAIN_TAG=CLANG38
+
+cd BootShim
+make UEFI_BASE=0x9FC00000 UEFI_SIZE=0x00300000
+cd ..
+
+cat ./BootShim/BootShim.bin ./Build/SurfaceDuo1-AARCH64/DEBUG_CLANG38/FV/SM8150_EFI.fd > ./ImageResources/Epsilon/bootpayload.bin
+cat ./ImageResources/Epsilon/patchedkernel ./Build/SurfaceDuo1-AARCH64/DEBUG_CLANG38/FV/SM8150_EFI.fd > ./ImageResources/Epsilon/dualbootbootpayload.bin
+
+python3 ./ImageResources/mkbootimg.py \
+  --kernel ./ImageResources/Epsilon/bootpayload.bin \
+  --ramdisk ./ImageResources/Epsilon/ramdisk \
+  -o ./ImageResources/Epsilon/uefi.img \
+  --pagesize 4096 \
+  --header_version 2 \
+  --cmdline "console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xa90000 androidboot.hardware=surfaceduo androidboot.hardware.platform=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 loop.max_part=7 androidboot.usbcontroller=a600000.dwc3 kpti=off buildvariant=user" \
+  --dtb ./ImageResources/Epsilon/dtb \
+  --base 0x0 \
+  --os_version 11.0.0 \
+  --os_patch_level 2023-08-01 \
+  --second_offset 0xf00000
+
+python3 ./ImageResources/mkbootimg.py \
+  --kernel ./ImageResources/Epsilon/dualbootbootpayload.bin \
+  --ramdisk ./ImageResources/Epsilon/ramdisk \
+  -o ./ImageResources/Epsilon/uefi_dualboot.img \
+  --pagesize 4096 \
+  --header_version 2 \
+  --cmdline "console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xa90000 androidboot.hardware=surfaceduo androidboot.hardware.platform=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 loop.max_part=7 androidboot.usbcontroller=a600000.dwc3 kpti=off buildvariant=user" \
+  --dtb ./ImageResources/Epsilon/dtb \
+  --base 0x0 \
+  --os_version 11.0.0 \
+  --os_patch_level 2023-08-01 \
+  --second_offset 0xf00000
---
+++ build_zeta.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+export CLANG38_BIN=/usr/lib/llvm-14/bin/ && export CLANG38_AARCH64_PREFIX=aarch64-linux-gnu-
+
+python3 ./Platforms/SurfaceDuo2Pkg/PlatformBuild.py --build TOOL_CHAIN_TAG=CLANG38
+
+cd BootShim
+make UEFI_BASE=0x9FC00000 UEFI_SIZE=0x00300000
+cd ..
+
+cat ./BootShim/BootShim.bin ./Build/SurfaceDuo2-AARCH64/DEBUG_CLANG38/FV/SM8350_EFI.fd > ./ImageResources/Zeta/bootpayload.bin
+cat ./ImageResources/Zeta/patchedkernel ./Build/SurfaceDuo2-AARCH64/DEBUG_CLANG38/FV/SM8350_EFI.fd > ./ImageResources/Zeta/dualbootbootpayload.bin
+
+python3 ./ImageResources/mkbootimg.py \
+  --kernel ./ImageResources/Zeta/bootpayload.bin \
+  --ramdisk ./ImageResources/Zeta/ramdisk \
+  -o ./ImageResources/Zeta/uefi.img \
+  --pagesize 4096 \
+  --header_version 3 \
+  --cmdline "" \
+  --base 0x0 \
+  --os_version 11.0.0 \
+  --os_patch_level 2023-08-01
+
+python3 ./ImageResources/mkbootimg.py \
+  --kernel ./ImageResources/Zeta/dualbootbootpayload.bin \
+  --ramdisk ./ImageResources/Zeta/ramdisk \
+  -o ./ImageResources/Zeta/uefi_dualboot.img \
+  --pagesize 4096 \
+  --header_version 3 \
+  --cmdline "" \
+  --base 0x0 \
+  --os_version 11.0.0 \
+  --os_patch_level 2023-08-01
