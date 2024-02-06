7799db5728f91f17e9ad8a40d75e2c965391673d
Gustave Monce <gustave.monce@outlook.com>
2024-02-07 00:02:33 +0100

    DBG

--- build_fd_zeta.sh
+++ build_fd_zeta.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Andromeda888 FD
-python3 ./Platforms/SurfaceDuo2Pkg/PlatformBuild.py TARGET=RELEASE
-mv ./Build/SurfaceDuo2Pkg/RELEASE_CLANGDWARF/FV/SM8350_EFI.fd ./ImageResources/Zeta/SM8350_EFI.fd
+python3 ./Platforms/SurfaceDuo2Pkg/PlatformBuild.py TARGET=DEBUG
+mv ./Build/SurfaceDuo2Pkg/DEBUG_CLANGDWARF/FV/SM8350_EFI.fd ./ImageResources/Zeta/SM8350_EFI.fd
 
 # Andromeda888 FD (SecureBoot Disabled)
-python3 ./Platforms/SurfaceDuo2Pkg/PlatformBuildNoSb.py TARGET=RELEASE
-mv ./Build/SurfaceDuo2Pkg/RELEASE_CLANGDWARF/FV/SM8350_EFI.fd ./ImageResources/Zeta/SM8350_EFI_NOSB.fd
+python3 ./Platforms/SurfaceDuo2Pkg/PlatformBuildNoSb.py TARGET=DEBUG
+mv ./Build/SurfaceDuo2Pkg/DEBUG_CLANGDWARF/FV/SM8350_EFI.fd ./ImageResources/Zeta/SM8350_EFI_NOSB.fd
