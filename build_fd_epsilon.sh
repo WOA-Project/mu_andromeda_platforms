0cf1f654c6b3e6b31effb333c24c78909b547cbe
Gustave Monce <gustave.monce@outlook.com>
2024-02-21 21:39:51 +0100

    Revert "Bump MU_BASECORE from `4da6e70` to `0dd0d48` (#1000)"
    This reverts commit 7519173233c9871d9803b23caca6a036b1f01d20.

--- build_fd_epsilon.sh
+++ build_fd_epsilon.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Andromeda855 FD
-python3 ./Platforms/SurfaceDuo1Pkg/PlatformBuild.py TARGET=DEBUG
-mv ./Build/SurfaceDuo1Pkg/DEBUG_CLANGDWARF/FV/SM8150_EFI.fd ./ImageResources/Epsilon/SM8150_EFI.fd
+python3 ./Platforms/SurfaceDuo1Pkg/PlatformBuild.py TARGET=RELEASE
+mv ./Build/SurfaceDuo1Pkg/RELEASE_CLANGDWARF/FV/SM8150_EFI.fd ./ImageResources/Epsilon/SM8150_EFI.fd
 
 # Andromeda855 FD (SecureBoot Disabled)
-python3 ./Platforms/SurfaceDuo1Pkg/PlatformBuildNoSb.py TARGET=DEBUG
-mv ./Build/SurfaceDuo1Pkg/DEBUG_CLANGDWARF/FV/SM8150_EFI.fd ./ImageResources/Epsilon/SM8150_EFI_NOSB.fd
+python3 ./Platforms/SurfaceDuo1Pkg/PlatformBuildNoSb.py TARGET=RELEASE
+mv ./Build/SurfaceDuo1Pkg/RELEASE_CLANGDWARF/FV/SM8150_EFI.fd ./ImageResources/Epsilon/SM8150_EFI_NOSB.fd
--- build_setup.sh
+++ build_setup.sh
@@ -8,5 +8,5 @@ sudo apt install -y uuid-dev clang llvm gcc-aarch64-linux-gnu
 
 pip install --upgrade -r ./pip-requirements.txt
 
-python3 ./Platforms/SurfaceDuo1Pkg/PlatformBuild.py --setup -t DEBUG
-python3 ./Platforms/SurfaceDuo1Pkg/PlatformBuild.py --update -t DEBUG
+python3 ./Platforms/SurfaceDuo1Pkg/PlatformBuild.py --setup -t RELEASE
+python3 ./Platforms/SurfaceDuo1Pkg/PlatformBuild.py --update -t RELEASE
