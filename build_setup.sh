a9e7059d838526468388c449ae0040cc0f951716
Gustave Monce <gustave.monce@outlook.com>
2025-08-30 22:44:55 +0200

    Move package and Python setup to build_setup.sh
    Consolidates installation of additional packages and Python dependencies from azure-pipelines.yml into build_setup.sh. This streamlines CI setup and applies a workaround for nuget issues on newer Ubuntu versions.

--- build_setup.sh
+++ build_setup.sh
@@ -6,6 +6,12 @@ sudo apt install -y uuid-dev clang llvm gcc-aarch64-linux-gnu lld
 #cargo install --force cargo-make
 #cargo add cargo-tarpaulin
 
+# Workaround an issue with bundled nuget and new enough Ubuntu versions (like the one used in the CI)
+sudo sh -c 'echo "deb http://archive.ubuntu.com/ubuntu jammy main universe" > /etc/apt/sources.list.d/jammy.list'
+sudo apt-get update
+sudo apt-get install -y mono-complete nuget make
+
+# Install and Upgrade all needed python packages
 pip install --upgrade -r ./pip-requirements.txt
 
 python3 ./Platforms/SurfaceDuo1Pkg/PlatformBuild.py --setup -t RELEASE
