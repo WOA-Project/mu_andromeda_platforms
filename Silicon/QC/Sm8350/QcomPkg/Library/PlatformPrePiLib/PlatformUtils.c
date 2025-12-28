20f7d0d811ba9d63f76d13bf1d101a25ff06fdf3
Gustave Monce <gustave.monce@outlook.com>
2025-12-28 14:07:40 +0100

    Cleanup PlatformUtils

--- Silicon/QC/Sm8350/QcomPkg/Library/PlatformPrePiLib/PlatformUtils.c
+++ Silicon/QC/Sm8350/QcomPkg/Library/PlatformPrePiLib/PlatformUtils.c
@@ -7,9 +7,9 @@
 **/
 #include <PiPei.h>
 
+#include <Library/ConfigurationMapHelperLib.h>
 #include <Library/IoLib.h>
 #include <Library/PlatformPrePiLib.h>
-#include <Library/ConfigurationMapHelperLib.h>
 
 #include "PlatformUtils.h"
 
@@ -30,8 +30,7 @@ VOID PlatformInitialize(VOID)
   UINT32 EarlyInitCoreCnt = 2;
   LocateConfigurationMapUINT32ByName("EarlyInitCoreCnt", &EarlyInitCoreCnt);
 
-  for (int i = 0; i < EarlyInitCoreCnt; i++)
-  {
+  for (int i = 0; i < EarlyInitCoreCnt; i++) {
     // Wake up redistributor for CPU i
     MmioWrite32(
         GICR_WAKER_CPU(i),
